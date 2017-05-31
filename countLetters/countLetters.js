2480ad56424534cdecb6627e42f8b0a4fae76ff9
dbrechin <dbrechin@live.ca>
2017-05-31 16:24:07 +0000

    finally done this damn task

---
+++ countLetters/countLetters.js
@@ -0,0 +1,21 @@
+var arg = process.argv[2]
+
+
+
+function countLetters(str) {
+  var str = str.split('');
+  var letterCount = {};
+
+  for (var char = 0; char < str.length; char++){
+    var current = letterCount[str[char]];
+    if (current == undefined) {
+      current = 1;
+    }
+    else {
+      current ++;
+    }
+    letterCount[str[char]] = current;
+  }
+return letterCount;
+}
+console.log(countLetters(arg));
